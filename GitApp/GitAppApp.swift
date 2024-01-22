a24684623aedac91efac4f28b5b19afe44e715da
Islam <aitemr16@gmail.com>
2024-01-22 11:06:49 +0600

    Initial Commit

---
+++ GitApp/GitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitAppApp.swift
+//  GitApp
+//
+//  Created by Islam Temirbek on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
